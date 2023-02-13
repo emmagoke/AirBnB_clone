8f057d72ce430e5b37e976245a1c9d429dbda62f
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-02-12 16:56:50 -0800

    Added the project files

--- console.py
+++ console.py
@@ -10,3 +10,33 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """Console"""
     prompt = '(hbnb)'
+
+    def do_create(self, line):
+        """
+        Creates a new instance of BaseModel, saves it
+        (to the JSON file)
+        """
+        if line == "BaseModel":
+            obj = eval("models.base_model" + '.' + "{}()".format(line))
+            print(obj.id)
+
+    def do_EOF(self, arg):
+        """
+        End of File.
+        """
+        return True
+
+    def emptyline(self):
+        """This method handle situation where empty Lines is
+        typed into the console."""
+        if self.lastcmd:
+            self.lastcmd = ""
+            return self.onecmd('\n')
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        sys.exit()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
