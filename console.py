dbfde71e99930d2b4248ca4d18a66581ae249a6d
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-02-12 16:08:00 -0800

    Added a code that passes the pycodestyle checks

---
+++ console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This Module contains the entry point of the command interpreter
+"""
+import sys
+import cmd
+#  import models
+
+
+class HBNBCommand(cmd.Cmd):
+    """Console"""
+    prompt = '(hbnb)'
